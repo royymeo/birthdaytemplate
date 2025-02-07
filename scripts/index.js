26c15d668574182a1953bf44b9dee3c786f9981f
royymeo <royhankamilputra@gmail.com>
2025-02-08 01:43:49 +0700

    update scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 9, 2020',
+  birthdate: 'Feb 9, 2025',
   name: 'AFIF AZIZI'
 };
 
